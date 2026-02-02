1899e1dcb2f412d486c0903c47d1b8b1d375f6d5
Nolwenn Dubourg <nold14@hotamil.fr>
2026-02-02 17:47:24 -0300

    ajout exemple text chaque sections

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -5,11 +5,41 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='banner'>
         <h1>DUBOURG Nolwenn</h1>
         <h2>Développeur front-end</h2>
         <p>Je suis passionnée par la création de sites web et d'applications interactives.</p>
-      </div>     
+      </div>
+      <div className='about'>
+        <h2>À propos de moi</h2>
+        <p>Bienvenue sur mon portfolio ! Je m'appelle Nolwenn Dubourg et je suis développeur front-end. J'adore transformer des idées en expériences numériques attrayantes et fonctionnelles. </p>
+      </div>
+      <div className='projects'>
+        <h2>Projets</h2>
+          <h3>Projet 1</h3>
+          <h3>Projet 2</h3>
+          <h3>Projet 3</h3>
+      </div>
+      <div className='skills'>
+        <h2>Compétences</h2>
+          <h3>HTML</h3>
+          <h3>CSS</h3>
+          <h3>JavaScript</h3>
+          <h3>React</h3>
+          <h3>Node.js</h3>
+          <h3>MongoDB</h3>
+          <h3>Git</h3>
+          <h3>Figma</h3>
+      </div>
+      <div className='education'>
+        <h2>Formation</h2>
+          <h3>Baccalauréat scientifique</h3>
+          <h3>BTS électrotechnique</h3>
+          <h3>Intégrateur Web (openclassrooms)</h3>
+      </div>
+      <div className='contact'>
+        <h2>Contact</h2>
+      </div>
     </>
   )
 }
