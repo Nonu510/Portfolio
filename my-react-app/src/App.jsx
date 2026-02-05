fa31071dd3b9b7300ff284a46cdd3ea94753f2a7
Nolwenn Dubourg <nold14@hotamil.fr>
2026-02-05 16:23:19 -0300

    changement image banière et ajout texte et style

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css'
 import './styles.css'
-import Banner from './components/banner/banner'
+import Banner from './components/banner/banner.jsx'
 
 function App() {
 
@@ -10,36 +10,38 @@ function App() {
       <div className='banner' id='home'>
         <Banner />
       </div>
-      <div className='about' id='about'>
-        <h2>À propos de moi</h2>
-        <p>Bienvenue sur mon portfolio ! Je m'appelle Nolwenn Dubourg et je suis développeur front-end. J'adore transformer des idées en expériences numériques attrayantes et fonctionnelles. </p>
-      </div>
-      <div className='projects' id='projects'>
-        <h2>Projets</h2>
-          <h3>Projet 1</h3>
-          <h3>Projet 2</h3>
-          <h3>Projet 3</h3>
-      </div>
-      <div className='skills' id='skills'>
-        <h2>Compétences</h2>
-          <h3>HTML</h3>
-          <h3>CSS</h3>
-          <h3>JavaScript</h3>
-          <h3>React</h3>
-          <h3>Node.js</h3>
-          <h3>MongoDB</h3>
-          <h3>Git</h3>
-          <h3>Figma</h3>
-          <h3>SCSS</h3>
-      </div>
-      <div className='education' id='education'>
-        <h2>Formation</h2>
-          <h3>Baccalauréat scientifique</h3>
-          <h3>BTS électrotechnique</h3>
-          <h3>Intégrateur Web (openclassrooms)</h3>
-      </div>
-      <div className='contact' id='contact'>
-        <h2>Contact</h2>
+      <div className='main-content'>
+        <div className='about' id='about'>
+          <h2>À propos de moi</h2>
+          <p>Bienvenue sur mon portfolio ! Je m'appelle Nolwenn Dubourg et je suis développeur front-end. J'adore transformer des idées en expériences numériques attrayantes et fonctionnelles. </p>
+        </div>
+        <div className='projects' id='projects'>
+          <h2>Projets</h2>
+            <h3>Projet 1</h3>
+            <h3>Projet 2</h3>
+            <h3>Projet 3</h3>
+        </div>
+        <div className='skills' id='skills'>
+          <h2>Compétences</h2>
+            <h3>HTML</h3>
+            <h3>CSS</h3>
+            <h3>JavaScript</h3>
+            <h3>React</h3>
+            <h3>Node.js</h3>
+            <h3>MongoDB</h3>
+            <h3>Git</h3>
+            <h3>Figma</h3>
+            <h3>SCSS</h3>
+        </div>
+        <div className='education' id='education'>
+          <h2>Formation</h2>
+            <h3>Baccalauréat scientifique</h3>
+            <h3>BTS électrotechnique</h3>
+            <h3>Intégrateur Web (openclassrooms)</h3>
+        </div>
+        <div className='contact' id='contact'>
+          <h2>Contact</h2>
+        </div>
       </div>
     </>
   )
--- my-react-app/src/components/banner/banner.jsx
+++ my-react-app/src/components/banner/banner.jsx
@@ -4,9 +4,9 @@ function Banner() {
   return (
     <div className="hero">
         <section className="hero-content">
-          <h1 className="sr-only">DUBOURG Nolwenn</h1>
-          <p className="subtitle">Développeur front-end</p>
-          <p className="text">Je suis passionnée par la création de sites web et d'applications interactives.</p>
+          <h1 className="sr-only">Bonjour et bienvenue sur mon portfolio!<br /> Je suis <span>Nolwenn</span></h1>
+          {/* <p className="subtitle">Développeur front-end</p> */}
+          <p className="text"><span>Développeur front-end</span> passionnée par la création de sites web et d'applications interactives.</p>
         </section>
     </div>
   )
